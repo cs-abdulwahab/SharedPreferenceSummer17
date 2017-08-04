7d7e994a51aa68d90749358acc5cbf37da5880f8
Abdul Wahab <cs.abdulwahab@gmail.com>
2017-08-04 09:18:59 +0500

    Shared Preference

--- app/src/main/java/com/example/abdul_wahab/sharedpreferencesummer17/MainActivity.java
+++ app/src/main/java/com/example/abdul_wahab/sharedpreferencesummer17/MainActivity.java
@@ -1,13 +1,46 @@
 package com.example.abdul_wahab.sharedpreferencesummer17;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText etInput;
+    Button btnSave;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etInput = (EditText) findViewById(R.id.etInput);
+        btnSave = (Button) findViewById(R.id.btnSave);
+
+        //Singleton Design Pattern
+        final SharedPreferences sp = getSharedPreferences("mysettings", MODE_PRIVATE);
+
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String input = etInput.getText().toString();
+                SharedPreferences.Editor editor = sp.edit();
+                editor.putString("input", input);
+                editor.commit();
+            }
+        });
+
+
+        String s = sp.getString("input", null);
+        if (s == null) {
+            etInput.setText("First time login");
+        } else {
+            etInput.setText(s);
+        }
+
+
     }
 }
